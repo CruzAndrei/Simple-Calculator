d635523eff6d99c1cb0659e6569834134c343c0e
CruzAndrei <cruzandrei237@gmail.com>
2023-08-04 12:14:52 +0800

    the calculator basic operations

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,5 +1,11 @@
 #basic math operations
+print("Here are the basic math operation to be used:")
+print("+ ADD")
+print("- SUBTRACT")
+print("* MULTIPLY")
+print("/ DIVIDE")
 #ADD
 #subtract
 #multiply
 #divide
+
